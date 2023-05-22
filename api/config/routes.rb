86a852eefd9e82f1971e2d73644d5ac9d3d75b68
Gustavo Reis <gustavoreissilva.2016@gmail.com>
2023-05-21 21:11:29 -0300

    add controllers files

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/api', defaults: { format: :json } do
+    resources :categories, only: [:index]
+    resources :restaurants, only: [:index, :show]
+    resources :orders, only: [:create, :show]
+    resources :available_cities, only: [:index]
+  end
 end
